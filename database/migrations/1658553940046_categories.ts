a25940909f6e78834c3ebbf3dad3401ccd9c70d7
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-07-23 09:59:45 +0200

    feat: create categories (#2)
    * feat: create categories
    
    * feat: add messages in update
    
    * fix: update from review

---
+++ database/migrations/1658553940046_categories.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'categories'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('slug', 255).notNullable().unique()
+      table.string('name', 255).notNullable().unique()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
