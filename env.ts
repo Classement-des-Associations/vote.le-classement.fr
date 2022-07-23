5f2fc188724f277fcaa4147a744ed664b2a8c6b9
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-07-23 07:20:06 +0200

    fix: add a rules on pg host

--- env.ts
+++ env.ts
@@ -23,6 +23,7 @@ export default Env.rules({
   SESSION_DRIVER: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  PG_HOST: Env.schema.string({ format: 'host' }),
   PG_PORT: Env.schema.number(),
   PG_USER: Env.schema.string(),
   PG_PASSWORD: Env.schema.string.optional(),
