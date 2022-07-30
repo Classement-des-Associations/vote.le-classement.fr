cf1a0c13f2acb421c8bddf700d0735a96fa45783
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-07-30 13:10:32 +0200

    feat: add links to edit image and document

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,25 @@ import focus from '@alpinejs/focus'
 
 Alpine.plugin(focus)
 
+Alpine.data('dropdown', () => ({
+  open: false,
+  toggle() {
+    if (this.open) {
+      return this.close()
+    }
+
+    this.$refs.button.focus()
+
+    this.open = true
+  },
+  close(focusAfter) {
+    if (!this.open) return
+
+    this.open = false
+
+    focusAfter && focusAfter.focus()
+  },
+}))
+
 window.Alpine = Alpine
 Alpine.start()
