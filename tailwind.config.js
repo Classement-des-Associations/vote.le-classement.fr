7e4ded3636aa499c45310b81c34d8c16ed1d619a
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-08-03 18:56:46 +0200

    fix: force images to be square

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,27 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./resources/views/**/*.edge', './resources/js/**/*.js'],
   theme: {
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: '10',
+      11: '11',
+      12: '12',
+      13: '13',
+      14: '14',
+      15: '15',
+      16: '16',
+    },
     extend: {
       colors: {
         'primary': {
@@ -24,5 +45,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
