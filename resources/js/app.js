846a5b64d30de87e3d9b25f92385ca2e3a20d8c0
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-08-03 08:58:15 +0200

    fix: enable navigate with teleport

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,26 @@ import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
 import collapse from '@alpinejs/collapse'
 
+up.fragment.config.mainTargets.push('[layout-main]')
+
+window.Alpine = Alpine
+
 Alpine.plugin(focus)
 Alpine.plugin(collapse)
+
+document.addEventListener('alpine:initialized', () => {
+  // Select all anchors with the attribut 'up-target'
+  const anchors = document.querySelectorAll('[mobile-nav] a[up-target]')
+  // Prevent default behavior for all anchors (used for x-teleport)
+  for (const anchor of anchors) {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault()
+      const target = anchor.getAttribute('up-target')
+      const url = anchor.getAttribute('href')
+      up.navigate({ target, url })
+    })
+  }
+})
 
 Alpine.data('dropdown', () => ({
   open: false,
@@ -29,5 +47,4 @@ Alpine.data('dropdown', () => ({
   },
 }))
 
-window.Alpine = Alpine
 Alpine.start()
